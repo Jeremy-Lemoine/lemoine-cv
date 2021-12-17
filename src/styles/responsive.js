293ca46de4da70436d442b664e15fd6bba35f1f1
Jérémy <jeremyreouven@hotmail.fr>
2021-12-17 22:15:51 +0100

    Old data in responsive file - fixed

--- src/styles/responsive.js
+++ src/styles/responsive.js
@@ -10,13 +10,7 @@ class Field {
 	}
 }
 
-const css = {
-	"--navigation-bar-height": new Field("120px", "calc(50px + 2vh)"),
-	"--navbar-item-width": new Field("150px", "150px"),
-	"--navbar-item-text-font-size": new Field("calc(15px + 0.2vh)", "calc(15px + 0.2vh)"),
-	"--title-font-size": new Field("35px", "25px"),
-	"--title-icon-size": new Field("25px", "calc(30px + 0.2vh)"),
-};
+const css = {};
 
 const updateCSS = (typeScreen) => {
 	Object.entries(css).forEach(([variable, field]) => {
