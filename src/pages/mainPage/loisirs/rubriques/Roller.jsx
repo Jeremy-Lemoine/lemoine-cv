e103a93f629bebcf96f57d4ee889e515ca3be6f5
Jérémy <jeremyreouven@hotmail.fr>
2022-02-02 17:41:45 +0100

    Rollers section

--- src/pages/mainPage/loisirs/rubriques/Roller.jsx
+++ src/pages/mainPage/loisirs/rubriques/Roller.jsx
@@ -1,3 +1,10 @@
 export default function Roller() {
-	return <span>C'est plus qu'un sport, c'est une <important-word>philosophie</important-word>.</span>;
+	return (
+		<span>
+			C'est plus qu'un sport, c'est une <important-word>philosophie</important-word>.
+			<br />
+            <br />
+			C'est un peu comme des chaussons à la longue.
+		</span>
+	);
 }
